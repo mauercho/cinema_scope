8502b293cadf91aad86f75e915da895d6a3fba8e
soralee <pineapple223663@gmail.com>
2024-05-20 11:42:53 +0900

    profile image

--- final-pjt-back/pjt_back/urls.py
+++ final-pjt-back/pjt_back/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
     path('movies/', include('movies.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
