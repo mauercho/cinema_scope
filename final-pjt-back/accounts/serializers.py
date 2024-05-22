61d08f71f1c537a3d4a9bab5958327c02416222f
soralee <pineapple223663@gmail.com>
2024-05-22 11:01:08 +0900

    Review serializer 수정

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile, User
+from movies.models import Movie, Review
 from movies.serializers import MovieSimpleSerializer, ReviewSerializer
 from django.conf import settings
 
@@ -9,7 +10,19 @@ class ProfileSerializer(serializers.ModelSerializer):
     class UserSerializer(serializers.ModelSerializer):
         favorites = MovieSimpleSerializer(many=True, read_only=True)
         like_movies = MovieSimpleSerializer(many=True, read_only=True)
-        review_set = ReviewSerializer(many=True, read_only=True)
+
+        class ReviewListSerializer(serializers.ModelSerializer):
+            class MovieTitleSerializer(serializers.ModelSerializer):
+                class Meta:
+                    model = Movie
+                    fields = ('title',)
+            movie = MovieTitleSerializer(read_only=True)
+
+            class Meta:
+                model = Review
+                fields = '__all__'
+        review_set = ReviewListSerializer(many=True, read_only=True)
+
         class Meta:
             model = User
             fields = ('favorites', 'like_movies', 'review_set')
